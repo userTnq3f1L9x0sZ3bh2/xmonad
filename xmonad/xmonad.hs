1427a74df899c2fa3cf854798fa3011850d68878
userTnq3f1L9x0sZ3bh2 <andrr5t7@proton.me>
2026-08-30 02:25:42 +0000

    Add Alacritty to workspace management

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -25,7 +25,7 @@ myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 myManageHook :: ManageHook
 myManageHook = composeAll
-       [ className =? "st" --> doShift "1"
+       [ className =? "alacritty" --> doShift "1"
        , className =? "firefox"   --> doShift "2"
        , className =? "librewolf" --> doShift "3"
        , className =? "vmware"    --> doShift "5"
